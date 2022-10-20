14d85318a95c96bf901815d48a536c5cadfe6e97
Mario Rodriguez <marioa.rodriguez@msn.com>
2022-10-19 20:17:19 -0600

    First release of nestjs task project

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(Id: string): Task {
+    return this.tasks.find((task) => task.id === Id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
